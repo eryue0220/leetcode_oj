d0ba474e02d056d15c332f5bb951123ddfc0b87a
= <chenshigang1@letv.com>
2017-02-09 16:58:24 +0800

    update.

--- array/insert.js
+++ array/insert.js
@@ -4,7 +4,7 @@ var error = false;
 
 function insert(array, pos, elem) {
   if (array.length === maxsize) return error;
-  if (pos < 1 || pos > array.length + 1) return error;
+  if (pos < 0 || pos > array.length + 1) return error;
 
   if (pos <= array.length) {
     for (var k = array.length - 1; k >= pos - 1; k--) {
