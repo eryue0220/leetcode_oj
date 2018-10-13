7d4964d2d6b6e10f579f928d72df865d0b4e85ba
chenshigang <chenshigang@meituan.com>
2018-10-13 17:05:49 +0800

    solve leetcode 283

--- array/283_move_zeros.js
+++ array/283_move_zeros.js
@@ -3,10 +3,18 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-  for (var i = 0; i < nums.length; i++) {
+  var length = nums.length;
+  var i = 0;
+  while (length && i < nums.length) {
     if (nums[i] === 0) {
       nums.splice(i, 1);
       nums.push(0);
+    } else {
+      i += 1;
     }
+
+    length -= 1;
   }
+
+  console.log(nums);
 };
